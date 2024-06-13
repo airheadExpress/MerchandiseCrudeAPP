c6128a130b91309368c6b344b28fa7c83de6b7ac
Tristan Gonzales <cruztristan175@gmail.com>
2024-06-13 16:10:33 +0800

    Add project files.

---
+++ MerchandiseCrudeAPI/FrmMerchandisecs.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace MerchandiseCrudeAPI
+{
+    partial class FrmMerchandisecs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.dgMerchandises = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMerchandises)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(337, 47);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(173, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(337, 73);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(173, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(337, 99);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(173, 20);
+            this.txtDesc.TabIndex = 2;
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(337, 125);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(173, 20);
+            this.txtQty.TabIndex = 3;
+            // 
+            // dgMerchandises
+            // 
+            this.dgMerchandises.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMerchandises.Location = new System.Drawing.Point(60, 227);
+            this.dgMerchandises.Name = "dgMerchandises";
+            this.dgMerchandises.Size = new System.Drawing.Size(691, 197);
+            this.dgMerchandises.TabIndex = 4;
+            this.dgMerchandises.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgMerchandises_RowHeaderMouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Merch ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Merch Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Merch Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Merch Quantity";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(206, 175);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 35);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(307, 175);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 35);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(407, 175);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 35);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(507, 175);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 35);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // FrmMerchandisecs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgMerchandises);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Name = "FrmMerchandisecs";
+            this.Text = "FrmMerchandisecs";
+            this.Load += new System.EventHandler(this.FrmMerchandisecs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgMerchandises)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.DataGridView dgMerchandises;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ MerchandiseCrudeAPI/FrmMerchandisecs.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MerchandiseCrudeAPI
+{
+    public partial class FrmMerchandisecs : Form
+    {
+        public FrmMerchandisecs()
+        {
+            InitializeComponent();
+        }
+
+        MerchDBEntities data = new MerchDBEntities();
+        public static int Id = -1;
+
+        private void FrmMerchandisecs_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
+        }
+
+        public void LoadData() 
+        {
+            List<MerchTable> merchandises = new List<MerchTable>();
+            merchandises = data.MerchTables.ToList();
+            dgMerchandises.DataSource = merchandises;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            //int qtyPie = Int32.Parse(txtQty.Text);
+            MerchTable merch = new MerchTable();
+            merch.merchName = txtName.Text;
+            merch.merchDescription = txtDesc.Text;
+            merch.merchQuantity = txtQty.Text;
+
+            data.MerchTables.Add(merch);
+            data.SaveChanges();
+            MessageBox.Show("Data Added");
+            LoadData();
+        }
+
+        private void dgMerchandises_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            Id = Convert.ToInt32(dgMerchandises.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtName.Text = dgMerchandises.Rows[e.RowIndex].Cells[1].Value == DBNull.Value ? "" : dgMerchandises.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtDesc.Text = dgMerchandises.Rows[e.RowIndex].Cells[2].Value == DBNull.Value ? "" : dgMerchandises.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtQty.Text = dgMerchandises.Rows[e.RowIndex].Cells[3].Value == DBNull.Value ? "" : dgMerchandises.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (Id > 0) 
+            {
+                MerchTable merch = data.MerchTables.Where(m => m.merchID == Id).SingleOrDefault();
+                merch.merchName = txtName.Text;
+                merch.merchDescription = txtDesc.Text;
+                merch.merchQuantity = txtQty.Text;
+
+                data.SaveChanges();
+                MessageBox.Show("Update Success");
+
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgMerchandises.SelectedRows.Count > 0) 
+            {
+                foreach (DataGridViewRow row in dgMerchandises.SelectedRows) 
+                {
+                    int id = (int)row.Cells[0].Value;
+                    var merch = data.MerchTables.SingleOrDefault(x => x.merchID == id);
+                    data.MerchTables.Remove(merch);
+                    data.SaveChanges();
+                }
+                MessageBox.Show("Data Deleted");
+            }
+            LoadData();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "") 
+            {
+                List<MerchTable> merchandises = new List<MerchTable>();
+                merchandises = data.MerchTables.Where(m => m.merchName.Contains(txtName.Text)).ToList();
+                dgMerchandises.DataSource = merchandises;
+            }
+        }
+    }
+}
